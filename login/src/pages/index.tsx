44e00f015de9767ac404149739e856bd488fdefa
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-04-24 09:27:36 +0300

    added base things

---
+++ login/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <>
+      <Link href={"/register"}>register</Link>
+      <Link href={"/login"}>login</Link>
+    </>
+  )
+}
