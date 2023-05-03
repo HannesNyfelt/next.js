9c5e6ec5e4c39fed50db4a0c201cd428e4d6c4b6
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-05-03 09:03:22 +0300

    added base things

---
+++ plus_minus/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <button id="plus">Plus</button>
+        <button id="minus">Minus</button>
+        <button id="noll">Noll</button>
+      </div>
+    </>
+  )
+}
