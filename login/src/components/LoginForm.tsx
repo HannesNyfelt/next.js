2a8e819b80dd77d5b56a0d5e127da53bb09dffa8
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-05-05 12:28:21 +0300

    somthing

--- login/src/components/LoginForm.tsx
+++ login/src/components/LoginForm.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import { log } from "console";
+import React, { FormEvent, useState } from "react";
 
 const LoginForm = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [visable, setVisable] = useState('')
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault()
+        fetchData()
+    }
+    async function fetchData() {
+        const res = await fetch('http://10.111.3.78:3000/api/users')
+        const data = await res.json()
+        console.log(data);
+        if (data.includes(email))
+            console.log('yes');
+
+    }
+
     return (
         <>
             <div>
