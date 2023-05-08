de9a652e3058db81794bb245f557de344dc63644
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-05-08 12:29:34 +0300

    Done

--- dice_again/src/pages/index.tsx
+++ dice_again/src/pages/index.tsx
@@ -1,29 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [nums, setNums] = useState<number[]>([]);
+
+  function click() {
+    setNums([
+      Math.floor(Math.random() * 5 + 1),
+      Math.floor(Math.random() * 5 + 1)
+    ]);
+  }
+
   return (
     <>
       <div id="div">
+        {nums.map((num, i) => {
+          return <img key={i} src={`/images/die${num}.png`} />;
+        })}
       </div>
-
-      <button className="button" role="button" id="btn">Roll The Dice</button>
-
+      <button onClick={click} className="button" role="button" id="btn">
+        Roll The Dice
+      </button>
     </>
-  )
+  );
 }
-
-const btn = document.querySelector("#btn") as HTMLButtonElement
-const div = document.querySelector("#div") as HTMLDivElement
-let die = document.createElement("img")
-let die2 = document.createElement("img")
-
-
-btn.addEventListener("click", function () {
-  div.append(die)
-  const num = Math.floor(Math.random() * 5 + 1)
-  die.src = `images/die${num}.png`
-
-  div.append(die2)
-  const num2 = Math.floor(Math.random() * 5 + 1)
-  die2.src = `images/die${num2}.png`
-})
