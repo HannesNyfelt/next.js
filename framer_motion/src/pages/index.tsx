3b9c7844933a4966f89695378f818040d9ea96a6
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-05-16 12:52:10 +0300

    Done

--- framer_motion/src/pages/index.tsx
+++ framer_motion/src/pages/index.tsx
@@ -30,7 +30,25 @@ const Wireframe = () => {
             <motion.svg whileHover={{ scale: 1.15 }} whileTap={{ scale: 0.95 }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6">
               <path fill-rule="evenodd" d="M10.707 3.293a1 1 0 010 1.414L6.414 9H16a1 1 0 010 2H6.414l4.293 4.293a1 1 0 11-1.414 1.414l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 0z" clip-rule="evenodd" />
             </motion.svg>
-            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.99 }} className="bg-gray-400 w-1/2 mt-8 mb-20 h-80"></motion.div>
+            <motion.div className="bg-gray-400 w-1/2 mt-8 mb-20 h-80"
+              whileHover={{
+                scale: 1.1
+              }}
+              whileTap={{
+                scale: 0.99
+              }}
+              initial={{
+                opacity: 0,
+                scale: 0
+              }}
+              whileInView={{
+                opacity: 1,
+                scale: 1,
+                transition: {
+                  duration: 1
+                }
+              }}
+            ></motion.div>
             <motion.svg whileHover={{ scale: 1.15 }} whileTap={{ scale: 0.95 }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6">
               <path fill-rule="evenodd" d="M9.293 16.707a1 1 0 01-1.414-1.414L12.586 10H4a1 1 0 010-2h8.586l-4.293-4.293a1 1 0 011.414-1.414l6 6a1 1 0 010 1.414l-6 6z" clip-rule="evenodd" />
             </motion.svg>
@@ -39,7 +57,24 @@ const Wireframe = () => {
           <div className="flex flex-wrap gap-2 justify-center items-center">
             {Array.from(Array(40)).map((_, index) => (
               <React.Fragment key={index}>
-                <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className='flex flex-col mb-10 shadow'>
+                <motion.div className='flex flex-col mb-10 shadow'
+                  whileHover={{
+                    scale: 1.1
+                  }}
+                  whileTap={{
+                    scale: 0.9
+                  }}
+                  initial={{
+                    opacity: 0,
+                    y: '2rem'
+                  }}
+                  whileInView={{
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                      duration: 1
+                    }
+                  }}>
                   <div className={`bg-gray-400 mb-4 mt-10 h-40 w-60 m-2 ${index % 6 === 5 ? 'mb-2' : ''}`}></div>
                   <div className='flex w-full px-4 justify-between'>
                     <div className='h-12 w-12 rounded-full bg-gray-400'></div>
