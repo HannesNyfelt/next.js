acbc75b338ff3fc2d4794f787e04a97200db173c
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-05-23 08:42:13 +0300

    done

--- word_counter/src/pages/index.tsx
+++ word_counter/src/pages/index.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Home() {
-  return (
-    <>
-      <body className="flex items-center justify-center">
-        <div>
-          <input type="text" className="text-black" />
-          <p>words: <span>0</span></p>
-          <p>letters: <span>0</span></p>
-          <p>letters without space: <span>0</span></p>
-        </div>
-      </body>
+  const [inputText, setInputText] = useState("");
+  const wordCount = inputText.trim().split(" ").filter(word => word !== "").length;
+  const letterCount = inputText.length;
+  const letterCountWithoutSpaces = inputText.replace(/\s/g, "").length;
 
-    </>
-  )
+  const handleInputChange = (e: any) => {
+    setInputText(e.target.value);
+  };
+
+  return (
+    <body className="flex items-center justify-center">
+      <div>
+        <input
+          type="text"
+          className="text-black"
+          value={inputText}
+          onChange={handleInputChange}
+        />
+        <p>words: <span>{wordCount}</span></p>
+        <p>letters: <span>{letterCount}</span></p>
+        <p>letters without space: <span>{letterCountWithoutSpaces}</span></p>
+      </div>
+    </body>
+  );
 }
