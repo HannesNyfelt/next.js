4bbf398d85cf1b85f4917906557a4c51105678c9
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-08-11 09:50:01 +0300

    done

--- cards/src/app/layout.tsx
+++ cards/src/app/layout.tsx
@@ -1,11 +1,11 @@
 import './globals.css'
-import type { Metadata } from 'next'
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import type { Metadata } from 'next'
 import { CssBaseline } from '@mui/material';
 
 export const metadata: Metadata = {
