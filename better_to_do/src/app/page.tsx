12fff064f74f9ed9049a7d65cf0f2f63a826beef
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-08-24 09:28:55 +0300

    Done

--- better_to_do/src/app/page.tsx
+++ better_to_do/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { TextField, Button, Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import { Card, TextField, Typography, Button, Box, Table, TableBody, TableContainer, TableHead, TableRow } from "@mui/material"
 import DeleteIcon from "@mui/icons-material/Delete"
 import React, { useState, useEffect } from 'react'
 
@@ -8,12 +8,15 @@ interface Todo {
   id: number;
   completed: boolean;
   text: string;
+  createdAt: number;
 }
 
 
 export default function Home() {
   const [todos, setTodos] = useState<Todo[]>([])
   const [newTodoText, setNewTodoText] = useState<string>('')
+  const [editTodoId, setEditTodoId] = useState<number | null>(null)
+  const [editTodoText, setEditTodoText] = useState<string>('')
 
   useEffect(() => {
     const storedTodos = localStorage.getItem('todos')
@@ -32,6 +35,7 @@ export default function Home() {
         id: Date.now(),
         completed: false,
         text: newTodoText,
+        createdAt: Date.now()
       }
 
       setTodos(prevTodos => [...prevTodos, newTodo])
@@ -46,6 +50,24 @@ export default function Home() {
       )
     )
   }
+
+  function startEdit(id: number, text: string) {
+    setEditTodoId(id)
+    setEditTodoText(text)
+  }
+
+  function saveEdit() {
+    if (editTodoId !== null) {
+      setTodos(prevTodos =>
+        prevTodos.map(todo =>
+          todo.id === editTodoId ? { ...todo, text: editTodoText } : todo
+        )
+      )
+      setEditTodoId(null)
+      setEditTodoText('')
+    }
+  }
+
 
   function deleteTodo(id: number) {
     setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
@@ -75,18 +97,26 @@ export default function Home() {
           <Table sx={{ minWidth: 500 }}>
             <TableHead>
               <TableRow>
-                <TableCell align="left">Check</TableCell>
-                <TableCell align="left">Name</TableCell>
-                <TableCell align="right">Delete</TableCell>
               </TableRow>
             </TableHead>
-            <TableBody>
+            <TableBody sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
               {todos.map(todo => (
-                <TableRow key={todo.id}>
-                  <TableCell><input type="checkbox" checked={todo.completed} onChange={() => toggleTodo(todo.id)}></input></TableCell>
-                  <TableCell><span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>{todo.text}</span></TableCell>
-                  <TableCell align="right"><DeleteIcon onClick={() => deleteTodo(todo.id)}></DeleteIcon></TableCell>
-                </TableRow>
+                <Card sx={{ width: '20%', padding: 2, margin: 2 }} key={todo.id}>
+                  {editTodoId === todo.id ? (
+                    <>
+                      <TextField value={editTodoText} onChange={e => setEditTodoText(e.target.value)}></TextField>
+                      <Button onClick={saveEdit}>Save</Button>
+                    </>
+                  ) : (
+                    <>
+                      <input type="checkbox" checked={todo.completed} onChange={() => toggleTodo(todo.id)}></input>
+                      <Typography style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>{todo.text}</Typography>
+                      <Typography fontSize={13} color={"gray"} >Created: {new Date(todo.createdAt).toLocaleString()}</Typography>
+                      <Button onClick={() => startEdit(todo.id, todo.text)}>Edit</Button>
+                      <DeleteIcon onClick={() => deleteTodo(todo.id)}></DeleteIcon>
+                    </>
+                  )}
+                </Card>
               ))}
             </TableBody>
           </Table>
