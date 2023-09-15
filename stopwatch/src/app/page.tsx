4d67adad090194ba22dc5adc116683fa37025bf4
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-09-15 08:42:19 +0300

    Added a clicker

--- stopwatch/src/app/page.tsx
+++ stopwatch/src/app/page.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react'
 import { Box, Button, Typography } from '@mui/material'
 
 export default function Home() {
+
   //clock
   const [date, setDate] = useState(new Date())
 
@@ -44,7 +45,47 @@ export default function Home() {
     setTime(0);
   };
 
+  //Clicker
+  const [points, setPoints] = useState(0)
+  const [pointsPerSecond, setPointsPerSecond] = useState(0)
+  const [pointsPerClick, setPointsPerClick] = useState(1)
 
+  const [clickUpgradeCost, setClickUpgradeCost] = useState(10)
+  const [secondUpgradeCost, setSecondUpgradeCost] = useState(10)
+  const [clickUpgrade, setClickUpgrade] = useState(0)
+  const [secondUpgrade, setSecondUpgrade] = useState(0)
+
+  function increment() {
+    setPoints(points + pointsPerClick)
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPoints(points + pointsPerSecond)
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [points, pointsPerSecond])
+
+  function buyClickUpgrade() {
+    if (points >= clickUpgradeCost) {
+      setPoints(points - clickUpgradeCost)
+      setClickUpgrade(clickUpgrade + 1)
+      setClickUpgradeCost(clickUpgradeCost * 2)
+      setPointsPerClick(pointsPerClick + 1)
+    }
+  }
+
+  function buySecondUpgrade() {
+    if (points >= secondUpgradeCost) {
+      setPoints(points - secondUpgradeCost)
+      setSecondUpgrade(secondUpgrade + 1)
+      setSecondUpgradeCost(secondUpgradeCost * 2)
+      setPointsPerSecond(pointsPerSecond + 1)
+    }
+  }
 
   return (
     <Box sx={{
@@ -52,8 +93,37 @@ export default function Home() {
       alignItems: 'center',
       justifyContent: 'center',
       flexDirection: 'column',
-      height: '90vh'
     }}>
+      <h1>Clicker</h1>
+      <h3>Points per click: {pointsPerClick}</h3>
+      <h3>Points per second: {pointsPerSecond}</h3>
+      <Typography variant='h1'>{points}</Typography>
+      <Button variant='contained' onClick={increment} >Click Here!</Button>
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '60%'
+      }}>
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column'
+        }}>
+          <Typography variant='h6'>Buy click upgrade</Typography>
+          <Button variant='contained' onClick={buyClickUpgrade}>{clickUpgradeCost}</Button>
+        </Box>
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column'
+        }}>
+          <Typography variant='h6'>Buy Second upgrade</Typography>
+          <Button variant='contained' onClick={buySecondUpgrade}>{secondUpgradeCost}</Button>
+        </Box>
+      </Box>
       <h1>Clock</h1>
       <Typography variant='h1'>
         {date.toLocaleTimeString()}
