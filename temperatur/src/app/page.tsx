eb2a3fa63af2cea3866bd206bd5f40a13a1b99ed
HannesNyfelt <hannes.nyfelt@gmail.com>
2023-09-28 08:59:31 +0300

    Done

--- temperatur/src/app/page.tsx
+++ temperatur/src/app/page.tsx
@@ -1,10 +1,37 @@
 "use client"
+import React, { useState } from 'react'
+import { TextField, Box, Typography } from '@mui/material'
 
-import Image from 'next/image'
+
 
 export default function Home() {
+
+  const [celsius, setCelsius] = useState('');
+  const [fahrenheit, setFahrenheit] = useState('');
+
+  const handleCelsiusChange = (value: any) => {
+    const converted = (value * 9 / 5) + 32;
+    setFahrenheit(converted || '' as any);
+  };
+
   return (
-    <>
-    </>
+    <main style={{
+      height: '90vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column'
+    }}>
+      <Typography variant='h1' >Temperature Converter</Typography>
+      <Box sx={{
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'space-evenly',
+        justifyContent: 'space-evenly'
+      }}>
+        <TextField type='number' value={celsius} onChange={(e) => setCelsius(e.target.value)} onBlur={(e) => handleCelsiusChange(e.target.value)} variant='outlined' label="Celcius"></TextField>
+        <TextField type='number' value={fahrenheit} onChange={(e) => setFahrenheit(e.target.value)} onBlur={(e) => { const converted = (e.target.value as any - 32) * 5 / 9; setCelsius(converted || '' as any) }} variant='outlined' label="Fahrenheit"></TextField>
+      </Box>
+    </main>
   )
 }
